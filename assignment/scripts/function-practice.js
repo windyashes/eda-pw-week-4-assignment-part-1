ffac642faf87c3eef72bb70400975fd2a0a1624a
windyashes <sethkemmet@gmail.com>
2024-08-06 12:21:49 -0500

    answered question 2

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,10 +15,10 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
-  return;
+  return `Hello, ${name}!`;
 }
 // Remember to call the function to test
-
+console.log(`helloName function: ${helloName('person')}`);
 
 // 3. Function to add two numbers together & return the result
 function addNumbers(firstNumber) {
